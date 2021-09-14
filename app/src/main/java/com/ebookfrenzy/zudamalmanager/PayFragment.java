4f3a586697e8332b092e1fd964fab5a479286b7b
khush-byte <khush4459@gmail.com>
2021-09-14 17:57:23 +0500

    Pay action on going (job finished)

--- app/src/main/java/com/ebookfrenzy/zudamalmanager/PayFragment.java
+++ app/src/main/java/com/ebookfrenzy/zudamalmanager/PayFragment.java
@@ -1,8 +1,11 @@
 package com.ebookfrenzy.zudamalmanager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
@@ -10,7 +13,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import com.ebookfrenzy.zudamalmanager.databinding.FragmentPayBinding;
+import com.ebookfrenzy.zudamalmanager.request.GetBalanceRequestUpdate;
+import com.ebookfrenzy.zudamalmanager.request.PayRequest;
 import com.ebookfrenzy.zudamalmanager.tools.DecimalDigitsInputFilter;
 
 public class PayFragment extends Fragment {
@@ -19,7 +26,7 @@ public class PayFragment extends Fragment {
 
     // TODO: Rename and change types of parameters
     private String mParam1;
-    private String mParam2;
+    private String mParam2, agentID;
     FragmentPayBinding binding;
 
     public PayFragment() {
@@ -68,10 +75,26 @@ public class PayFragment extends Fragment {
         });
         binding.actionTitle.setText(((FirstActivity) getActivity()).title);
 
-        binding.testBtn.setOnClickListener(new View.OnClickListener() {
+        SharedPreferences pref = getContext().getSharedPreferences("root_manager", 0);
+        String agent_info = pref.getString("agent_info", "");
+        Log.i("Debug", agent_info);
+        String[] text = agent_info.split("&");
+
+        binding.agentIdField.setText("ID"+text[0]);
+        binding.agentBalField.setText(text[2]+", "+text[3]);
+        agentID = text[0];
+
+        binding.actionPayBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("Debug", "Hello11");
+                if(binding.payField.getText().toString().length()>0 && !binding.payField.getText().toString().equals("0")) {
+                    /*PayRequest request = new PayRequest(PayFragment.this, binding.payField.getText().toString(), agentID);
+                    request.execute();*/
+                    Toast.makeText(getContext(), "Приложение не закончено!", Toast.LENGTH_SHORT).show();
+                    Navigation.findNavController(((FirstActivity) getActivity()), R.id.nav_host_fragment_content_first).navigate(R.id.actionFragment);
+                }else{
+                    Toast.makeText(getContext(), "Не правильная сумма", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/ebookfrenzy/zudamalmanager/request/PayRequest.java
+++ app/src/main/java/com/ebookfrenzy/zudamalmanager/request/PayRequest.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.ebookfrenzy.zudamalmanager.AgentsFragment;
+import com.ebookfrenzy.zudamalmanager.PayFragment;
 import com.ebookfrenzy.zudamalmanager.adapters.AgentAdapter;
 import com.ebookfrenzy.zudamalmanager.tools.HTTPHandler;
 import com.ebookfrenzy.zudamalmanager.tools.MyData;
@@ -17,24 +18,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PayRequest extends AsyncTask<Void, Void, Void> {
-    private String myResponse, user_name, hash;
-    AgentsFragment activity;
+    private String myResponse, user_name, hash, sum, id, password, string_hash;
+    PayFragment activity;
 
-    public PayRequest(AgentsFragment activity) {
+    public PayRequest(PayFragment activity, String sum, String id) {
         this.activity = activity;
+        this.sum = sum;
+        this.id = id;
     }
 
     @Override
     protected void onPreExecute() {
         SharedPreferences pref = activity.getContext().getSharedPreferences("root_manager", 0);
         user_name =  pref.getString("login", "");
-        hash = pref.getString("hash", "");
+        password = pref.getString("sign", "");
+        string_hash = pref.getString("hash", "");
+        hash = MD5(user_name+"1"+sum+id+"1"+password);
     }
 
     @Override
     protected Void doInBackground(Void... arg0) {
         HTTPHandler handler = new HTTPHandler();
-        myResponse = handler.makeServiceCall(myUrl + "agents.aspx?act=2&un="+user_name+"&hc="+hash);
+        myResponse = handler.makeServiceCall(myUrl + "pay.aspx?act=1&un="+user_name+"&sm="+sum+"&txd=1&aid="+id+"&hc="+hash);
+        Log.i("Debug", "pay.aspx?act=1&un="+user_name+"&sm="+sum+"&txd=1&aid="+id+"&hc="+hash);
         return null;
     }
 
@@ -43,5 +49,19 @@ public class PayRequest extends AsyncTask<Void, Void, Void> {
         if (myResponse != null) {
             Log.i("Hello", myResponse);
         }
+    }
+
+    public String MD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : array) {
+                sb.append(Integer.toHexString((b & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException ignored) {
+        }
+        return null;
     }
 }
